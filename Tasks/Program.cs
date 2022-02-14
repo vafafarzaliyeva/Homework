964ff5bbd5596d96e3f5998ed3f641bfe8ae0ee5
vafafarzaliyeva <vafahf@code.edu.az>
2022-02-14 22:59:54 +0300

    Last Commit

---
+++ Tasks/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+
+        #region Task1
+        // Gonderilmis 3 ededin en boyuyunu tapib qaytaran metod - 3 integer parametr qebul edir ve iclerinde en boyuk hansidirsa onu tapib return edir.
+
+        //    static void Main(string[] args)
+        //    {
+        //        Console.WriteLine("Ededleri daxil edin");
+
+        //        string eded1 = Console.ReadLine();
+        //        int num1 = int.Parse(eded1);
+
+        //        string eded2 = Console.ReadLine();
+        //        int num2 = int.Parse(eded2);
+
+        //        string eded3 = Console.ReadLine();
+        //        int num3 = int.Parse(eded3);
+
+
+
+        //        if (num1==num2 && num2 == num3)
+        //        {
+        //            Console.WriteLine("Beraberlik var");
+        //        }
+        //        else Console.WriteLine(TheLargestNumber(num1, num2, num3));
+
+
+        //    }
+
+        //    static int TheLargestNumber(int a , int b , int c)
+        //    {
+        //        if (a > b && b > c)
+        //        {
+        //            return a;
+        //        }
+        //        else if (b > a && a > c)
+        //        {
+        //            return b;
+        //        }
+        //        else return c;
+
+
+
+
+
+
+
+
+        //    }
+
+        #endregion
+        #region Task2
+        // Gonderilmis ededin reqemlerinin cemini qaytaran metod - misalcun bu metodu call ederken arqument olaraq 451 gondersek metoddan geriye 10 qayitmalidir.
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Ededi daxil et");
+        //    string eded = Console.ReadLine();
+        //    int num = int.Parse(eded);
+
+        //    Console.WriteLine(SumOfNumbers(num));
+
+
+        //}
+
+        //static int SumOfNumbers(int a)
+        //{
+        //    int k;
+        //    int cem = 0;
+        //    while (a > 0)
+        //    {
+        //        k = a % 10;
+        //        a /= 10; 
+        //        cem += k;
+        //    }
+        //    return cem;
+
+
+        //}
+
+        #endregion
+        #region Task3
+
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Mehsullarin qiymetini daxil edin");
+        //    string mehsul1 = Console.ReadLine();
+        //    double a = double.Parse(mehsul1);
+
+        //    string mehsul2 = Console.ReadLine();
+        //    double b = double.Parse(mehsul2);
+
+        //    string mehsul3 = Console.ReadLine();
+        //    double c = double.Parse(mehsul3);
+
+        //    Console.WriteLine(MarketCompany(a, b, c));
+
+
+        //}
+
+        //static double MarketCompany(double a , double b, double c)
+        //{
+          
+        //    if (a < b && a < c)
+        //    {
+        //        return c + b;
+        //    }
+        //    else if (b < a && b < c)
+        //    {
+        //        return a + c;
+
+        //    }
+        //    else return a + b;
+
+            
+
+
+
+
+
+        //}
+        #endregion
+    }
+
+}
